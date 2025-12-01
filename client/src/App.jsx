06e403e485ccf43166895886f266892bfceca2bb
lephu2k6 <lephu422@hotmail.com>
2025-12-01 22:49:44 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-200">
+      <h1 className="text-4xl font-bold text-white">Hello LEPHU!</h1>
+    </div>
+  );
+};
+export default App

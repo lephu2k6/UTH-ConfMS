33bb2f53c6760315bafbc27b19825f061a42ae79
Minh Phú <lephu422@hotmail.com>
2025-12-02 12:41:01 +0700

    Change greeting from 'Hello LEPHU!' to 'Hello world!'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-blue-200">
-      <h1 className="text-4xl font-bold text-white">Hello LEPHU!</h1>
+      <h1 className="text-4xl font-bold text-white">Hello world!</h1>
     </div>
   );
 };

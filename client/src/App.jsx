3ce9ac7a548fbd81148cfa41d1430e7a2fd4fe47
letanphucthichdichoi11-sudo <letanphucthichdichoi11@gmail.com>
2025-12-06 00:11:47 +0700

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-200">
+    <div className="">
       <h1 className="text-4xl font-bold text-white">Hello world!</h1>
     </div>
   );

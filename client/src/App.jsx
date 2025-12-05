377dd4136ea8b1ae5c496043ef5ec6ceababc421
hoanggivng <hoanggivng@gmail.com>
2025-12-06 00:25:04 +0700

    f

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@
 import React from "react";
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-200">
-      <h1 className="text-4xl font-bold text-white">Hello world!</h1>
-    </div>
+    <div>Helo</div>
   );
 };
 export default App
